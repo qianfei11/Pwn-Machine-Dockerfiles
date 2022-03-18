d38afbc8ec353e2db80abec83af9f430e6ac643c
qianfei11 <490584635@qq.com>
2022-03-18 13:37:18 +0800

    add the latest tag within gef & pwninit plugin

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 Ubuntu images for CTF PWNer, easy to use.
 
 ```bash
-$ docker push assassinq/pwn-machine:$TAGNAME
+$ docker pull assassinq/pwn-machine:$TAGNAME
 $ docker run -it --rm --privileged -e DISPLAY="$IP:0" -v "$PWD:/pwn" assassinq/pwn-machine:$TAGNAME /bin/zsh
 ```
+
+Using `latest` tag to get image with [gef](https://github.com/hugsy/gef) and [pwninit](https://github.com/io12/pwninit) plugin.
 
---
+++ latest/exp.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+from pwn import *
+
+context.log_level = 'debug'
+context.terminal = ['tmux', 'split', '-h']
+
+{bindings}
+
+context.binary = {bin_name}
+
+ENCODING = 'ISO-8859-1'
+s = lambda senddata : p.send(senddata.encode(ENCODING))
+sa = lambda recvdata, senddata : p.sendafter(recvdata.encode(ENCODING), senddata.encode(ENCODING))
+sl = lambda senddata : p.sendline(senddata.encode(ENCODING))
+sla = lambda recvdata, senddata : p.sendlineafter(recvdata.encode(ENCODING), senddata.encode(ENCODING))
+r = lambda numb=0x3f3f3f3f, timeout=0x3f3f3f3f : p.recv(numb, timeout=timeout).decode(ENCODING)
+ru = lambda recvdata, timeout=0x3f3f3f3f : p.recvuntil(recvdata.encode(ENCODING), timeout=timeout).decode(ENCODING)
+uu32 = lambda data : u32(data.encode(ENCODING), signed="unsigned")
+uu64 = lambda data : u64(data.encode(ENCODING), signed="unsigned")
+iu32 = lambda data : u32(data.encode(ENCODING), signed="signed")
+iu64 = lambda data : u64(data.encode(ENCODING), signed="signed")
+up32 = lambda data : p32(data, signed="unsigned").decode(ENCODING)
+up64 = lambda data : p64(data, signed="unsigned").decode(ENCODING)
+ip32 = lambda data : p32(data, signed="signed").decode(ENCODING)
+ip64 = lambda data : p64(data, signed="signed").decode(ENCODING)
+
+local = 1
+if local:
+    p = process({proc_args})
+else:
+    p = remote('localhost', 8888)
+
+gdb.attach(p)
+
+p.interactive()
